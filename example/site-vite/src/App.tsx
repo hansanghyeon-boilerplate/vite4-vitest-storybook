ce0613a5ff17ae346366642224486b19538cfa7a
Hansanghyeon <hyeon@imweb.me>
2022-12-20 18:40:29 +0900

    feat: example

---
+++ example/site-vite/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from '@lib/design-system'
+
+function App() {
+  return (
+    <div className="App">
+      <Button label="Button" primary />
+    </div>
+  )
+}
+
+export default App
