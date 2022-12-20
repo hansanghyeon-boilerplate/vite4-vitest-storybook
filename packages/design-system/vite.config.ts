627d96de308ead515c064c36d707c461b5bf42a7
Hansanghyeon <hyeon@imweb.me>
2022-12-20 17:25:00 +0900

    feat: init yarn lib

---
+++ packages/design-system/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default () => {
+  return defineConfig({
+    plugins: [
+      react(),
+      dts({
+        insertTypesEntry: true,
+      })
+    ],
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'MyLib',
+        formats: ['es', 'cjs', 'umd'],
+        fileName: (format) => `index.${format}.js`,
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
+        }
+      }
+    }
+  })
+}
+
+
